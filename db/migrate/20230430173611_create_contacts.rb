ce50b82edfd42825b1d9c354b65ad6f47d2a10e9
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-04-30 17:53:26 +0000

    lab1

--- db/migrate/20230430173611_create_contacts.rb
+++ db/migrate/20230430173611_create_contacts.rb
@@ -1,11 +1,11 @@
 class CreateContacts < ActiveRecord::Migration[7.0]
   def change
     create_table :contacts do |t|
-      t.string "first_name"
+      t.string "first_name" #"." is caled a method
       t.string "last_name"
       t.string "email"
       t.integer "company_id"
-      
+
       t.timestamps
     end
   end
