c49ae4a88ed76430571feee0dea6bfb5d04113be
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-05-01 18:47:49 +0000

    code along 3 done

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -12,6 +12,47 @@ Contact.destroy_all
 
 # 1. insert new rows in the contacts table with relationship to a company
 
+puts "Companies: #{Company.all.count}"
+
+puts "Contacts: #{Contact.all.count}"
+
+apple = Company.find_by("name" => "Apple")
+
+contact = Contact.new
+contact["first_name"] = "Tim"
+contact["last_name"] = "Cook"
+contact["email"] = "tim@apple.com"
+contact["company_id"] = apple["id"] #pulls di from apple hash
+contact.save
+
+puts contact.inspect
+
+contact = Contact.new
+contact["first_name"] = "Craig"
+contact["last_name"] = "Feddy"
+contact["email"] = "craig@apple.com"
+contact["company_id"] = apple["id"] #pulls di from apple hash
+contact.save
+
+
+amazon = Company.find_by("name" => "Amazon")
+bezos = Contact.new
+bezos["first_name"] = "Jeff"
+bezos["last_name"] = "Bezos"
+bezos["email"] = "jeff@blue.com"
+bezos["company_id"] = amazon["id"] #pulls di from apple hash
+bezos.save
+
+puts "Contacts: #{Contact.all.count}"
 # 2. How many contacts work at Apple?
 
+apple_contacts = Contact.where({"company_id" => apple["id"]})
+
+puts "Apple Contacts: #{apple_contacts.count}"
+
 # 3. What is the full name of each contact who works at Apple?
+for x in apple_contacts
+    first_name = x["first_name"]
+    last_name = x["last_name"]
+    puts "#{first_name} #{last_name}"
+end
